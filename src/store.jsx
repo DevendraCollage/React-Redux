6d046a9a5e96c4c90f014196541c637bcadd9d6f
Devendra Parmar <23030501043@darshan.ac.in>
2024-11-12 14:51:54 +0530

    Implement the Redux Store in The Redux Project how to add and remove items items from the Store

--- src/store.jsx
+++ src/store.jsx
@@ -1,11 +1,15 @@
+import { createStore } from "redux";
+
 /* eslint-disable no-case-declarations */
 const ADD_TASK = "task/add";
 const DELETE_TASK = "task/delete";
 
 const initialState = {
   task: [],
+  isLoading: false,
 };
 
+// Step:1 Create simple reducer function
 const taskReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TASK:
@@ -27,3 +31,20 @@ const taskReducer = (state = initialState, action) => {
       return state;
   }
 };
+
+// Step:2 Create the Redux store using the reducer
+const store = createStore(taskReducer);
+
+// Step:3 Log the initial state
+// The getState method is a synchronous function that returns the current state of a Redux application. It includes the entire state of the application, including all the reducers and their respective states.
+console.log("initial state", store.getState());
+
+// Step:4 Dispatch an action to add a task
+store.dispatch({ type: ADD_TASK, payload: "Redux Code" });
+console.log("updated state", store.getState());
+
+store.dispatch({ type: ADD_TASK, payload: "React Redux Code" });
+console.log("updated state", store.getState());
+
+store.dispatch({ type: DELETE_TASK, payload: 1 });
+console.log("deleted state", store.getState());
